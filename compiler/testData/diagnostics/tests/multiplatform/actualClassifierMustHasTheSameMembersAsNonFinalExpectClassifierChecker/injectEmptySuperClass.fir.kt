27c78512db4e52518c06de9e3fe9eda576739071
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-08-14 11:42:07 +0200

    fixup! K1: Implement a checker that disallows to have different member scopes for `expect open` and its `actual`

--- compiler/testData/diagnostics/tests/multiplatform/actualClassifierMustHasTheSameMembersAsNonFinalExpectClassifierChecker/injectEmptySuperClass.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/actualClassifierMustHasTheSameMembersAsNonFinalExpectClassifierChecker/injectEmptySuperClass.fir.kt
@@ -11,8 +11,6 @@ expect open class Foo {
 
 open class InjectedEmptySuperClass()
 
-// Hypothetically, it's more restricting than necessary. But technically, actual and expect scopes don't match (from some perspective).
-// If it wasn't reported, it wouldn't be cool that once you add a member to InjectedSuperClass, Foo would become red
 actual open class Foo : InjectedEmptySuperClass() {
     actual fun existingMethod() {}
     actual val existingParam: Int = 904
